1af4ed4fcf8faa51bbeff0a50a5cb65eb5931556
leslyndizey <l.ndizeye@alustudent.com>
2024-03-05 01:30:17 -0800

    API

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+import requests
+def get_emp_todo(emp_id):
+    URL = f"https//jsonplaceholder.typicode.com/users/{emp_id}/todos"
+    emp_todo = requests.get(URL).json{}
+    total_todos=len(emp_todo)
+    completed_todos = 0
+    for todo in emp todo:
+        if todo("completed"):
+            completed_todos += 1
+    print(f"Out of {total_todos} total, completed todos are: {completed_todos})
